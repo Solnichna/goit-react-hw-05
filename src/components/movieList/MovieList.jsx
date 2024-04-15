97e858dfc87d884bd2a33261ea04f19f3cf719b2
Solnichna <143127001+Solnichna@users.noreply.github.com>
2024-04-15 15:15:19 +0200

    Update MovieList.jsx

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,9 +1,16 @@
+import { Link, useLocation } from "react-router-dom";
 
 const MovieList = ({ movies }) => {
+
+  const location = useLocation();
+
   return (
     <ul>
       {movies.map(movie => (
-        <li key={movie.id}>{movie.title}</li>
+        <li key={movie.id}>
+         
+          <Link to={`${location.pathname}/${movie.id}`}>{movie.title}</Link>
+        </li>
       ))}
     </ul>
   );
